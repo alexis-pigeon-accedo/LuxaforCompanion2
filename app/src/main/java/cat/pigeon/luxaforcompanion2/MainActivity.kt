42ef6f88525ecf02e6d400256f933842659660eb
Alexis Pigeon <alexis.pigeon@accedo.tv>
2023-11-03 11:09:34 +0100

    Adding Jetpack Compose Preview capabilities
    * build.gradle.kts
     - added necessary dependencies
    * MainActivity.kt
     - added @Preview on top Composable

--- app/src/main/java/cat/pigeon/luxaforcompanion2/MainActivity.kt
+++ app/src/main/java/cat/pigeon/luxaforcompanion2/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cat.pigeon.luxaforcompanion2.ui.theme.LuxaforCompanion2Theme
 
@@ -42,6 +43,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
+    @Preview
     fun LuxaforButtons() {
         Column(
             modifier = Modifier.fillMaxWidth().background(Color.Black)
